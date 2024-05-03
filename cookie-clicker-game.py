18f129d52a821648660fc366350b32a44880e70e
charlie-fox-23 <charliefox931@gmail.com>
2024-05-03 10:16:57 -0600

    Fixed cookie image. Added background. Added cursor image and upgrade functionality. Now when we have enough cookies we can purchase a cursor.

--- cookie-clicker-game.py
+++ cookie-clicker-game.py
@@ -5,9 +5,26 @@ from pygame.constants import MOUSEBUTTONDOWN
 
 pygame.init()
 
+class Upgrade :
+  def __init__(self,image_path,position,cost):
+    self.image = pygame.image.load(image_path)
+    self.image = pygame.transform.scale(self.image,(50,50))
+    self.rect = self.image.get_rect(topleft = position)
+    self.cost = cost
+  def draw(self,screen):
+    screen.blit(self.image,self.rect)
+  def is_affordable(self,clicks):
+    return clicks >= self.cost
+  def purchase(self,cookie):
+    if cookie.click_count >= self.cost :
+      cookie.click_count -= self.cost
+      return True 
+    return False
+
 class Cookie :
-  def __init__(self,image_path,position):
+  def __init__(self,image_path,position,initial_size=(100,100)):
     self.original_image = pygame.image.load(image_path)
+    self.original_image = pygame.transform.scale(self.original_image,initial_size)
     self.image = self.original_image.copy()
     self.rect = self.image.get_rect(center = position)
     self.click_count = 0
@@ -18,7 +35,7 @@ class Cookie :
     self.click_count +=1
     self.image = pygame.transform.scale(self.original_image,(110,110))
     self.rect = self.image.get_rect(center = self.rect.center)
-    pygame.time.set_timer(pygame.USEREVENT,100)
+    pygame.time.set_timer(pygame.USEREVENT,200)
   def reset_size(self):
     self.image = self.original_image.copy()
     self.rect = self.image.get_rect(center = self.rect.center)
@@ -26,25 +43,38 @@ class Cookie :
 screen_width = 640
 screen_height = 480
 
+background_image = pygame.image.load("background.png")
+background_image = pygame.transform.scale(background_image,(screen_width,screen_height))
+
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
-cookie = Cookie("cookie.png",(screen_width//2,screen_height//2))
+cookie = Cookie("cookie.png",(screen_width//5,screen_height//2))
+upgrade = Upgrade("mouse_cursor.png",(screen_width//2,screen_height//10),16)
 running = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-    elif event.type == pygame.MOUSEBUTTONDOWN and cookie.rect.collidepoint(event.pos):
-      cookie.click()
+    elif event.type == pygame.MOUSEBUTTONDOWN: #and cookie.rect.collidepoint(event.pos):
+      if cookie.rect.collidepoint(event.pos):
+        cookie.click()
+      elif upgrade.rect.collidepoint(event.pos) and upgrade.is_affordable(cookie.click_count):
+        if upgrade.purchase(cookie):
+          print ("Upgrade purchased")
+          
     elif event.type == pygame.USEREVENT:
       cookie.reset_size()
-
-  screen.fill((255,255,255))
+  screen.blit(background_image,(0,0))
   cookie.draw(screen)
+  upgrade.draw(screen)
   font = pygame.font.Font(None,36)
   text = font.render(str(cookie.click_count),True,(0,0,0))
-  screen.blit(text,(screen_width//2-10,screen_height//2+60))
+  screen.blit(text,(screen_width//5-10,screen_height//2+60))
+  if upgrade.is_affordable(cookie.click_count):
+    upgrade_text = font.render("Click to buy",True,(0,128,0))
+  else:
+    upgrade_text = font.render(f"Cursor {upgrade.cost} cookies.",True,(255,0,0))
+  screen.blit(upgrade_text,(screen_width//2+60,screen_height//10))
   pygame.display.flip()
   clock.tick(60)
 pygame.quit()
-sys.exit()
